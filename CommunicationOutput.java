d3c74ffffeecacfb473d10f49fb23d55e122dad7
emgroussos <56846763+emgroussos@users.noreply.github.com>
2019-11-15 23:41:35 +0200

    Add the option to send messages to more employees

--- CommunicationOutput.java
+++ CommunicationOutput.java
@@ -2,46 +2,60 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CommunicationOutput {
+
 	public static ArrayList<Integer> senter = new ArrayList<Integer>();
 	public static ArrayList<Integer> receiver = new ArrayList<Integer>();
 	public static ArrayList<String> message = new ArrayList<String>();
-
+	
 	public void sentMessages(int id) {
-		int csent=0;
-		for (int i = 0; i < senter.size(); i++) {
-			if (senter.get(i).equals(id)) {
-				System.out.println("Message sent: " + message.get(i) + "\nMessage receiver was: " + receiver.get(i));
-				csent++;
-			} 
-			if (csent==0) {
-				System.out.println("User has never sent a message.");
+		for(int i = 0; i < senter.size(); i++) {
+			int no = 0;
+			if(senter.get(i).equals(id)) {
+				no++;
+				System.out.println("Message no: " + no);
+				System.out.println("Receiver's id: " + receiver.get(i));
+				System.out.println("Message:\n" +message.get(i) + "\n\n");
 			}
-
+			if(no == 0) {
+				System.out.println("There are no sent messages");
+			}
 		}
 	}
 
+	
 	public void receivedMessages(int id) {
-		int creceived=0;
-		for (int i = 0; i < receiver.size(); i++) {
+		int no = 0;
+		for(int i = 0; i < receiver.size(); i++){
 			if (receiver.get(i).equals(id)) {
-				System.out.println("Message sent: " + message.get(i) + "\n Message senter was: " + senter.get(i));
-				creceived++;
+				no++;
+				System.out.println("Message no: " + no);
+				System.out.println("Senter's id: " + senter.get(i));
+				System.out.print("Message:\n" +message.get(i) + "\n\n");
 			}
-			if (creceived==0) {
-				System.out.println("User has never receive a message.");
+			if(no == 0) {
+				System.out.println("There are no received messages");
 			}
 		}
 	}
-
+	
 	public void newMessage(int id) {
 		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please, write your message.");
+		String txt = in.next();
+		int answer;
+		do {
+		System.out.println("Please write the id of the employee you send the message to.");
+		int rid = in.nextInt();
 		senter.add(id);
-		System.out.print("Type message: ");
-		String txt = in.nextLine();
 		message.add(txt);
-		System.out.print("Enter receiver's id here: ");
-		int idr = in.nextInt();
-		receiver.add(idr);
-
+		receiver.add(rid);
+		System.out.println("Would you like to send the message to any other employees?(Y=1/N=2)");
+		answer = in.nextInt();
+		}while(answer == 1);
+		System.out.print("Your message has been sent succesfully.\n\n");
 	}
 }
+	
+	
+	
