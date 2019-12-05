094ba7b971850e91fcfb4ee45efed7aaa305260d
emgroussos <56846763+emgroussos@users.noreply.github.com>
2019-12-05 22:11:42 +0200

    Add files via upload
    adds comments and descriptions
    checks id inputs

--- CommunicationOutput.java
+++ CommunicationOutput.java
@@ -3,65 +3,76 @@ import java.util.Scanner;
 
 public class CommunicationOutput {
 
+	/* Creation of lists, in order to save ids and messages */
 	public static ArrayList<Integer> senter = new ArrayList<Integer>();
 	public static ArrayList<Integer> receiver = new ArrayList<Integer>();
 	public static ArrayList<String> message = new ArrayList<String>();
-	
+
+	/* This method returns the messages sent by the user */
 	public void sentMessages(int id) {
-		if(senter.size() == 0) {
+		if (senter.size() == 0) {
 			System.out.println("There are no sent messages\n");
 		}
-		for(int i = 0; i < senter.size(); i++) {
+		for (int i = 0; i < senter.size(); i++) {
 			int csent = 0;
-			if(senter.get(i).equals(id)) {
+			if (senter.get(i).equals(id)) {
 				csent++;
 				System.out.println("Message no: " + csent);
 				System.out.println("Receiver's id: " + receiver.get(i));
-				System.out.println("Message:\n" +message.get(i) + "\n\n");
+				System.out.println("Message:\n" + message.get(i) + "\n\n");
 			}
-			if(csent == 0) {
+			if (csent == 0) {
 				System.out.println("There are no sent messages\n");
 			}
 		}
 	}
 
-	
+	/* This method returns the messages that the user received */
 	public void receivedMessages(int id) {
 		int creceived = 0;
 		if (receiver.size() == 0) {
 			System.out.println("There are no receiver messages\n");
 		}
-		for(int i = 0; i < receiver.size(); i++){
+		for (int i = 0; i < receiver.size(); i++) {
 			if (receiver.get(i).equals(id)) {
 				creceived++;
 				System.out.println("Message no: " + creceived);
 				System.out.println("Senter's id: " + senter.get(i));
-				System.out.print("Message:\n" +message.get(i) + "\n\n");
+				System.out.print("Message:\n" + message.get(i) + "\n\n");
 			}
-			if(creceived == 0) {
+			if (creceived == 0) {
 				System.out.println("There are no received messages\n");
 			}
 		}
 	}
-	
+
+	/* This method creates and sends a new message */
 	public void newMessage(int id) {
 		Scanner in = new Scanner(System.in);
-		
 		System.out.println("Please, write your message.");
 		String txt = in.next();
-		String answer;
+		int rid;
+		int c = 0;
+		System.out.println("Please write the ids of the employees you send the message to.");
+		System.out.println("To stop press 0");
 		do {
-		System.out.println("Please write the id of the employee you send the message to.");
-		int rid = in.nextInt();
-		senter.add(id);
-		message.add(txt);
-		receiver.add(rid);
-		System.out.println("Would you like to send the message to any other employees?(Press yes, if so)");
-		answer = in.next();
-		}while(answer.toLowerCase().equals("y"));
+			do {
+			rid = in.nextInt();
+			if(rid == 0 && c != 0) {
+				break;
+			}
+			else if(rid == 0 && c == 0) {
+				System.out.println("You have to send the message to at least ne person. Please try again");
+			}
+			else if (Integer.toString(rid).length() != 8) {
+				System.out.println("ids have exactly 8 digits. Please try again");
+			}
+			}while(Integer.toString(rid).length() != 8 || rid == 0);
+			senter.add(id);
+			message.add(txt);
+			receiver.add(rid);
+			c++;
+		} while (rid != 0);
 		System.out.print("Your message has been sent succesfully.\n\n");
 	}
 }
-	
-	
-	
