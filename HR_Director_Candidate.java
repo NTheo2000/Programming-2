704e49ffca8486e70a73398d8433f1e7bf9f2eb8
miltos04 <56606550+miltos04@users.noreply.github.com>
2019-12-06 11:32:08 +0200

    Update HR_Director_Candidate.java

--- HR_Director_Candidate.java
+++ HR_Director_Candidate.java
@@ -2,14 +2,14 @@ import org.json.simple.JSONObject;
 
 public class HR_Director_Candidate extends HR_Employee_Candidate {
 
-	private static final int MULTIPLIER_FOR_EACH_MASTER_IN_MARKETING=10;
-	private static final int MULTIPLIER_FOR_EACH_PHD_IN_MARKETING=15;
+	private static final int MULTIPLIER_FOR_EACH_MASTER_IN_HUMAN_RESOURCES=10;
+	private static final int MULTIPLIER_FOR_EACH_PHD_IN_HUMAN_RESOURCES=15;
 	private static final int MULTIPLIER_FOR_EACH_MASTER_IN_OTHER_FIELD=5;
 	private static final int MULTIPLIER_FOR_EACH_PHD_IN_OTHER_FIELD=10;
 
-	private double[] master_in_marketing;
+	private double[] master_in_human_resources;
 	private double[] master_in_other_field;
-	private double[] phd_in_marketing;
+	private double[] phd_in_human_resources;
 	private double[] phd_in_other_field;
 	private int total_points;
 
@@ -19,9 +19,9 @@ public class HR_Director_Candidate extends HR_Employee_Candidate {
 
 	public HR_Director_Candidate(JSONObject candidate) {
 		super(candidate);
-		this.master_in_marketing = findDegreeAndField(candidate, "Master", "HR", true);
+		this.master_in_human_resources = findDegreeAndField(candidate, "Master", "HR", true);
 		this.master_in_other_field = findDegreeAndField(candidate, "Master", "HR", false);
-		this.phd_in_marketing = findDegreeAndField(candidate, "PhD", "HR", true);
+		this.phd_in_human_resources = findDegreeAndField(candidate, "PhD", "HR", true);
 		this.phd_in_other_field = findDegreeAndField(candidate, "PhD", "HR", false);
 		setTotal_points();
 	}
@@ -38,14 +38,14 @@ public class HR_Director_Candidate extends HR_Employee_Candidate {
 	@Override
 	protected int total_points() {
 		int sum = super.getTotal_points();
-		for(double i : master_in_marketing) {
-			sum += i * MULTIPLIER_FOR_EACH_MASTER_IN_MARKETING;
+		for(double i : master_in_human_resources) {
+			sum += i * MULTIPLIER_FOR_EACH_MASTER_IN_HUMAN_RESOURCES;
 		}
 		for(double i : master_in_other_field) {
 			sum += i * MULTIPLIER_FOR_EACH_MASTER_IN_OTHER_FIELD;
 		}
-		for(double i : phd_in_marketing) {
-			sum += i * MULTIPLIER_FOR_EACH_PHD_IN_MARKETING;
+		for(double i : phd_in_human_resources) {
+			sum += i * MULTIPLIER_FOR_EACH_PHD_IN_HUMAN_RESOURCES;
 		}
 		for(double i : phd_in_other_field) {
 			sum += i * MULTIPLIER_FOR_EACH_PHD_IN_OTHER_FIELD;
