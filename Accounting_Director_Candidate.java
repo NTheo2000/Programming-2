5f48def228ee63be86a7402f7e435e75cd822d46
miltos04 <56606550+miltos04@users.noreply.github.com>
2019-12-06 11:29:49 +0200

    Update Accounting_Director_Candidate.java

--- Accounting_Director_Candidate.java
+++ Accounting_Director_Candidate.java
@@ -2,14 +2,14 @@ import org.json.simple.JSONObject;
 
 public class Accounting_Director_Candidate extends Accounting_Employee_Candidate {
 
-	private static final int MULTIPLIER_FOR_EACH_MASTER_IN_MARKETING=10;
-	private static final int MULTIPLIER_FOR_EACH_PHD_IN_MARKETING=15;
+	private static final int MULTIPLIER_FOR_EACH_MASTER_IN_ACCOUNTING=10;
+	private static final int MULTIPLIER_FOR_EACH_PHD_IN_ACCOUNTING=15;
 	private static final int MULTIPLIER_FOR_EACH_MASTER_IN_OTHER_FIELD=5;
 	private static final int MULTIPLIER_FOR_EACH_PHD_IN_OTHER_FIELD=10;
 
-	private double[] master_in_marketing;
+	private double[] master_in_accounting;
 	private double[] master_in_other_field;
-	private double[] phd_in_marketing;
+	private double[] phd_in_accounting;
 	private double[] phd_in_other_field;
 	private int total_points;
 
@@ -19,9 +19,9 @@ public class Accounting_Director_Candidate extends Accounting_Employee_Candidate
 
 	public Accounting_Director_Candidate(JSONObject candidate) {
 		super(candidate);
-		this.master_in_marketing = findDegreeAndField(candidate, "Master", "Accounting", true);
+		this.master_in_accounting = findDegreeAndField(candidate, "Master", "Accounting", true);
 		this.master_in_other_field = findDegreeAndField(candidate, "Master", "Accounting", false);
-		this.phd_in_marketing = findDegreeAndField(candidate, "PhD", "Accounting", true);
+		this.phd_in_accounting = findDegreeAndField(candidate, "PhD", "Accounting", true);
 		this.phd_in_other_field = findDegreeAndField(candidate, "PhD", "Accounting", false);
 		setTotal_points();
 	}
@@ -38,14 +38,14 @@ public class Accounting_Director_Candidate extends Accounting_Employee_Candidate
 	@Override
 	protected int total_points() {
 		int sum = super.getTotal_points();
-		for(double i : master_in_marketing) {
-			sum += i * MULTIPLIER_FOR_EACH_MASTER_IN_MARKETING;
+		for(double i : master_in_accounting) {
+			sum += i * MULTIPLIER_FOR_EACH_MASTER_IN_ACCOUNTING;
 		}
 		for(double i : master_in_other_field) {
 			sum += i * MULTIPLIER_FOR_EACH_MASTER_IN_OTHER_FIELD;
 		}
-		for(double i : phd_in_marketing) {
-			sum += i * MULTIPLIER_FOR_EACH_PHD_IN_MARKETING;
+		for(double i : phd_in_accounting) {
+			sum += i * MULTIPLIER_FOR_EACH_PHD_IN_ACCOUNTING;
 		}
 		for(double i : phd_in_other_field) {
 			sum += i * MULTIPLIER_FOR_EACH_PHD_IN_OTHER_FIELD;
