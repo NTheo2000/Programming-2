223691a7b6b39208630bb6d35d012395b3e72ab4
miltos04 <56606550+miltos04@users.noreply.github.com>
2019-12-06 11:41:37 +0200

    Update Marketing_Director_Candidate.java

--- Marketing_Director_Candidate.java
+++ Marketing_Director_Candidate.java
@@ -1,7 +1,18 @@
+/*
+ * Name of class: Marketing_Director_Candidate
+ * 
+ * Copyright 2019 Miltiadis Tsichlis & Michael Dontas.
+ */
 import org.json.simple.JSONObject;
 
 public class Marketing_Director_Candidate extends Marketing_Employee_Candidate {
-
+/**
+	 * Searches a Json file for masters and phds in Marketing Science and other fields
+	 * The class reads a Json file and and finds key words in order to count the points for each candidate using fixed multipliers. 
+	 * 
+	 * @version 1.1 6 December 2019
+	 * @author Michael Dontas & Miltiadis Tsichlis
+	 */
 	private static final int MULTIPLIER_FOR_EACH_MASTER_IN_MARKETING=10;
 	private static final int MULTIPLIER_FOR_EACH_PHD_IN_MARKETING=15;
 	private static final int MULTIPLIER_FOR_EACH_MASTER_IN_OTHER_FIELD=5;
