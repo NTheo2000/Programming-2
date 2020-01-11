91fdba6afec42162452e147fe13b741e5e5c8986
Michael-Dontas <56359604+Michael-Dontas@users.noreply.github.com>
2020-01-12 00:50:37 +0200

    Made panes for viewing graphs

--- WindowDirectorCharts.java
+++ WindowDirectorCharts.java
@@ -1,4 +1,4 @@
-import java.awt.EventQueue;
+package GUI_Classes;
 
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
@@ -6,36 +6,32 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JLayeredPane;
 import javax.swing.SwingConstants;
+
+import EvaluateCandidates.Employee;
+import javafx.embed.swing.JFXPanel;
+
 import java.awt.Component;
 import javax.swing.Box;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Dimension;
 
-public class WindowDirectorCharts {
+public class WindowDirectorCharts extends LoginMenu {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					WindowDirectorCharts window = new WindowDirectorCharts();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private String firstname;
+	private Employee employee;
+	private JLayeredPane layeredPane;
 
 	/**
 	 * Create the application.
 	 */
-	public WindowDirectorCharts() {
+	public WindowDirectorCharts(String name, JFrame frame, Employee employee) {
+		this.frame = frame;
+		this.firstname = name;
+		this.employee = employee;
 		initialize();
 	}
 
@@ -43,12 +39,7 @@ public class WindowDirectorCharts {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane = new JLayeredPane();
 		frame.getContentPane().add(layeredPane, BorderLayout.CENTER);
 		layeredPane.setLayout(new BorderLayout(0, 0));
 		
@@ -67,27 +58,50 @@ public class WindowDirectorCharts {
 		Component horizontalStrut_1 = Box.createHorizontalStrut(123);
 		panel.add(horizontalStrut_1, BorderLayout.EAST);
 		
-		Component verticalStrut = Box.createVerticalStrut(20);
-		panel.add(verticalStrut, BorderLayout.NORTH);
-		
 		Component verticalStrut_1 = Box.createVerticalStrut(20);
 		panel.add(verticalStrut_1, BorderLayout.SOUTH);
+		
+		JPanel topPanel = new JPanel();
+		panel.add(topPanel, BorderLayout.NORTH);
+		topPanel.setLayout(new BorderLayout(0, 0));
+		
+		JButton backButton = setBackButton(topPanel);
+		backButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				layeredPane.setVisible(false);
+				String[] dep = employee.getDepartment().split("\\s");
+				if (dep[1].equals("Manager")) {
+					new EvEmpMenuforHR(firstname, frame, employee);
+				} else {
+					new DirectorMenu(firstname, frame, employee);
+				}
+			}
+		});
+		
+		Component verticalStrut = Box.createVerticalStrut(31);
+		topPanel.add(verticalStrut, BorderLayout.SOUTH);
 		
 		JPanel panel_1 = new JPanel();
 		layeredPane.add(panel_1, BorderLayout.WEST);
 		panel_1.setLayout(new BorderLayout(0, 0));
 		
 		JButton btnBarChart = new JButton("Score per question (Bar Chart)");
-		btnBarChart.setPreferredSize(new Dimension(150, 23));
+		btnBarChart.setPreferredSize(new Dimension(185, 23));
 		panel_1.add(btnBarChart, BorderLayout.CENTER);
+		btnBarChart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				layeredPane.setVisible(false);
+				showChart("Bar");			
+			}
+		});
 		
-		Component verticalStrut_2 = Box.createVerticalStrut(55);
+		Component verticalStrut_2 = Box.createVerticalStrut(15);
 		panel_1.add(verticalStrut_2, BorderLayout.NORTH);
 		
-		Component verticalStrut_3 = Box.createVerticalStrut(92);
+		Component verticalStrut_3 = Box.createVerticalStrut(108);
 		panel_1.add(verticalStrut_3, BorderLayout.SOUTH);
 		
-		Component horizontalStrut_2 = Box.createHorizontalStrut(8);
+		Component horizontalStrut_2 = Box.createHorizontalStrut(15);
 		panel_1.add(horizontalStrut_2, BorderLayout.WEST);
 		
 		Component horizontalStrut_3 = Box.createHorizontalStrut(0);
@@ -98,19 +112,53 @@ public class WindowDirectorCharts {
 		panel_2.setLayout(new BorderLayout(0, 0));
 		
 		JButton btnPieChart = new JButton("Employee satisfaction level (Pie Chart)");
-		btnPieChart.setPreferredSize(new Dimension(150, 23));
+		btnPieChart.setPreferredSize(new Dimension(220, 23));
 		panel_2.add(btnPieChart, BorderLayout.CENTER);
+		btnPieChart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				layeredPane.setVisible(false);
+				showChart("Pie");
+			}
+		});
 		
-		Component horizontalStrut_4 = Box.createHorizontalStrut(20);
+		Component horizontalStrut_4 = Box.createHorizontalStrut(26);
 		panel_2.add(horizontalStrut_4, BorderLayout.WEST);
 		
-		Component horizontalStrut_5 = Box.createHorizontalStrut(6);
+		Component horizontalStrut_5 = Box.createHorizontalStrut(10);
 		panel_2.add(horizontalStrut_5, BorderLayout.EAST);
 		
-		Component verticalStrut_4 = Box.createVerticalStrut(55);
+		Component verticalStrut_4 = Box.createVerticalStrut(16);
 		panel_2.add(verticalStrut_4, BorderLayout.NORTH);
 		
-		Component verticalStrut_5 = Box.createVerticalStrut(92);
+		Component verticalStrut_5 = Box.createVerticalStrut(108);
 		panel_2.add(verticalStrut_5, BorderLayout.SOUTH);
+	}
+	public void showChart(String chartType) {
+		JLayeredPane chartPane = new JLayeredPane();
+		frame.getContentPane().add(chartPane, BorderLayout.CENTER);
+		chartPane.setLayout(new BorderLayout(0, 0));
+		try {	
+				JFXPanel fxPanel = new JFXPanel();
+				chartPane.add(fxPanel, BorderLayout.CENTER);
+				if (chartType.equals("Bar")) {
+					fxPanel.setScene((new BarChartDirector(employee.getId())).generateChart());
+	        } else {
+				fxPanel.setScene((new PieChartEmployee(employee.getId())).generateChart());
+	        }
+	        JPanel topPanel = new JPanel();
+	        chartPane.add(topPanel, BorderLayout.NORTH);
+	        topPanel.setLayout(new BorderLayout(0, 0));
+	       				   		
+	   		JButton backButton = setBackButton(topPanel);
+	   		backButton.addActionListener(new ActionListener() {
+	   			public void actionPerformed(ActionEvent e) {
+	   				chartPane.setVisible(false);
+	   				layeredPane.setVisible(true);
+	   				frame.validate();
+	   			}
+	   		});
+		} catch (Exception e) {
+			endMessage("An error has occured", "DirectorMenu", frame);
+		}
 	}
 }
